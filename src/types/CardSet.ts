72b3a2e27a59fbd9678ea497d089bb94cb469519
harasuke <mirko.spinato@icloud.com>
2025-05-19 15:34:04 +0200

    Fix expansion name

--- src/types/CardSet.ts
+++ src/types/CardSet.ts
@@ -1,7 +1,7 @@
 export enum CardSet {
   GENETIC_APEX = 'Genetic Apex (A1)',
   MYTHICAL_ISLAND = 'Mythical Island (A1a)',
-  SPACE_TIME_SMACKDOWN = 'Space Time Smackdown (A2)',
+  SPACE_TIME_SMACKDOWN = 'Space-Time Smackdown (A2)',
   TRIUMPHANT_LIGHT = 'Triumphant Light (A2a)',
   SHINING_REVELRY = 'Shining Revelry (A2b)',
   CELESTIAL_GUARDIANS = 'Celestial Guardians (A3)',
